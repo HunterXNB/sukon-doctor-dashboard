420e691eb757d5f23923e18dca69f05cc6dccdac
Ahmed El-Sayad <poppop7517@gmail.com>
2025-05-11 11:22:51 +0300

    fixing group sidebar nav isssue

--- components/AppSidebar/NavigationItem.tsx
+++ components/AppSidebar/NavigationItem.tsx
@@ -55,7 +55,7 @@ function NavigationItem({
               className={cn(
                 "hover:bg-primary-50 hover:text-primary cursor-pointer select-none [&_svg]:text-[#6C6C89] [&_svg]:hover:text-primary [&_.fill-path]:hover:fill-primary [&_path:not(.fill-path)]:hover:stroke-primary",
                 {
-                  "data-[state=closed]:bg-primary-50 data-[state=closed]:text-primary data-[state=closed]:[&_svg]:text-primary data-[state=closed]:[&_.fill-path]:fill-primary data-[state=closed]:[&_path:not(.fill-path)]:stroke-primary":
+                  "data-[state=closed]:bg-primary-50 data-[state=closed]:text-primary [&[data-state='closed']_svg]:text-primary [&[data-state='closed']_.fill-path]:fill-primary [&[data-state='closed']_path:not(.fill-path)]:stroke-primary":
                     pathname.startsWith(item.href),
                 }
               )}
